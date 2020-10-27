3cc3db77e649cbc8f45e2249291df299b388d4fe
Cyriaque <yedagne.cyriaque@yahoo.fr>
2020-10-27 14:11:57 +0100

    Update movies.php

--- PHP/Day 4 - Working With Database/Example Files/movies.php
+++ PHP/Day 4 - Working With Database/Example Files/movies.php
@@ -2,7 +2,7 @@
 require_once 'Exercicedatabase.php';
 $db_name = 'moviedatabase';
 $db_found = mysqli_select_db($conn, $db_name);
-$imgUrl = "<img src= 'C:\xampp\htdocs\PHP\codingjobs\codingjobs\PHP\Day 4 - Working With Database\Fight-Club.jpg'>";
+//$imgUrl = "<img src= 'C:\xampp\htdocs\PHP\codingjobs\codingjobs\PHP\Day 4 - Working With Database\Fight-Club.jpg'>";
 if ($db_found) {
     echo "$db_name found !<br>";
     // Prepare my query
@@ -16,9 +16,9 @@ if ($db_found) {
         echo '<hr>';
         echo $db_record['title'] . '<br>';
         echo $db_record['yearofrelease'] . '<br>';
-        echo $db_record['poster'] . '<br>';
+        echo '<img src= "../images/' . $db_record['poster'] . '" width="250px" Height = "300px">';
     }
-
+    //echo '<img src="images/' . $movie['poster'] . '" width="100px">';
     //   $movies = mysqli_fetch_all($results, MYSQLI_ASSOC);
     //   echo '<pre>';
     //  var_dump($movies);
