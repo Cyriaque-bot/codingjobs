0208ee50b5cb072bb13163329f7db09bcaa30d94
Cyriaque <yedagne.cyriaque@yahoo.fr>
2020-10-28 14:44:03 +0100

    Update File_Exercise_1.php

--- PHP/Day 5 - Files_Session_Cookies_Encryption/Exercises/File_Exercise_1.php
+++ PHP/Day 5 - Files_Session_Cookies_Encryption/Exercises/File_Exercise_1.php
@@ -1,9 +1,20 @@
 <?php
 
-	/*
+/*
 		Create a script that saves in the last_access.log file the timestamp of this time (now).
 
 		We will replace the old timestamp.
 
 		Choose the right setting to open the file
 	 */
+
+//  Create the file 
+//fopen('books.txt', 'w');
+$logfile = fopen('last_access.log', 'w');;
+$da = strtotime('now');
+fwrite($logfile, $da);
+
+
+//fwrite(last_access . log, 'test');
+
+	 //echo date('Y/m/d H:i:s')
