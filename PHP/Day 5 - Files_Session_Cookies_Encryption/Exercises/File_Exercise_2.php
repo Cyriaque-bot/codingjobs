dfcb6a74c29e28c8daa8c1619c98939223408763
InsteanAzeros <fctinstean@gmail.com>
2020-10-28 10:10:35 +0100

    Exercises on files

---
+++ PHP/Day 5 - Files_Session_Cookies_Encryption/Exercises/File_Exercise_2.php
@@ -0,0 +1,15 @@
+<?php
+
+	/*
+		1. Create a file : 'signin.php'
+
+		2. In the 'signin.php' file, create a form with two input : email & password.
+
+		3. When user submit the form, you'll have to check :
+			- If this user actually exists.
+			- If he exists, check that both email/password are matching.
+		4. Display a message for both validations.
+
+		For this part, you'll use the 'users.txt' file.
+		TIPS : you can read the file and create an array of all the users...
+	 */
