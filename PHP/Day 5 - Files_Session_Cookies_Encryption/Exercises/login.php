ca1fb813e6fe8983d7eaa0504d345074e6ca292f
Cyriaque <yedagne.cyriaque@yahoo.fr>
2020-10-28 16:52:28 +0100

    Create login.php

---
+++ PHP/Day 5 - Files_Session_Cookies_Encryption/Exercises/login.php
@@ -0,0 +1,30 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+    $email = "";
+    $password = "";
+
+
+    ?>
+
+    <form action="" method="post">
+        <input type="text" name="email" placeholder="email" value="<php? echo " $email"?>" >
+        <input type="text" name="password" placeholder="password" value="<php? echo " $password"?>" >
+    </form>
+</body>
+
+</html>
