a5e94e85b4bc8dcd1f12202b72ba49058ee22ef8
InsteanAzeros <fctinstean@gmail.com>
2020-10-29 13:33:23 +0100

    Day 6 exercises

---
+++ PHP/Day 6 - Encryption_Injection/Exercises/PHP_Login_Register_2.php
@@ -0,0 +1,10 @@
+<?php
+	
+	/*
+		The purpose of this exercise is to edit the login process.
+		So you'll have to edit the code of previous exercise (Day 5 - Login exercise).
+	
+		Now you have to use 'password_verify' function to check the password !
+	*/
+
+?>
