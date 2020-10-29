322dbc92301e1cd4e906e07a650385bca91cef82
Cyriaque <yedagne.cyriaque@yahoo.fr>
2020-10-29 12:23:56 +0100

    Update Exercicedatabase.php

--- PHP/Day 5 - Files_Session_Cookies_Encryption/Exercises/Exercicedatabase.php
+++ PHP/Day 5 - Files_Session_Cookies_Encryption/Exercises/Exercicedatabase.php
@@ -2,7 +2,7 @@
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_SERVER', 'localhost');
-define('DB_NAME', 'moviedatabase');
+define('DB_NAME', 'users');
 
 
-$conn = mysqli_connect('localhost', 'root', '', 'moviedatabase');
+$conn = mysqli_connect('localhost', 'root', '', 'users');
