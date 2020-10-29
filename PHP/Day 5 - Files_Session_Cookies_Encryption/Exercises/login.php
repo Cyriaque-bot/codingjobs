736d852ef8102c877bc946f86f01a2b8b5420f36
Cyriaque <yedagne.cyriaque@yahoo.fr>
2020-10-29 09:21:36 +0100

    Update login.php

--- PHP/Day 5 - Files_Session_Cookies_Encryption/Exercises/login.php
+++ PHP/Day 5 - Files_Session_Cookies_Encryption/Exercises/login.php
@@ -17,13 +17,16 @@
     <?php
     $email = "";
     $password = "";
+    $submit = "";
 
 
     ?>
 
     <form action="" method="post">
-        <input type="text" name="email" placeholder="email" value="<php? echo " $email"?>" >
-        <input type="text" name="password" placeholder="password" value="<php? echo " $password"?>" >
+        <input type="text" name="email" placeholder="Email" value="<?php echo " $email" ?>"> <br><br>
+        <input type="text" name="password" placeholder="Password" value="<?php echo " $password" ?>"><br><br>
+
+        <input type="submit" name="Submission">
     </form>
 </body>
 
