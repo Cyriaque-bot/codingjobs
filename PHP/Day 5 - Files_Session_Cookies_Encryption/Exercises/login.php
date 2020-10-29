f7e9a9eeff6902e59be87739917354830cc61041
Cyriaque <yedagne.cyriaque@yahoo.fr>
2020-10-29 12:23:59 +0100

    Update login.php

--- PHP/Day 5 - Files_Session_Cookies_Encryption/Exercises/login.php
+++ PHP/Day 5 - Files_Session_Cookies_Encryption/Exercises/login.php
@@ -15,24 +15,47 @@
 <body>
 
     <?php
-    $email = "";
-    $password = "";
-    $submit = "";
 
-    session_start();
-
-    if (isset($submit))
-        if (isset($_SESSION['page_view']))
-            $_SESSION['page_view'] += 1;
-        else
-            $_SESSION['page_view'] = 1;
+    require_once 'Exercicedatabase.php';
 
 
+
+    //  $email = "";
+    // $password = "";
+    //$submit = "";
+
+    // = 'SELECT * 
+    //FROM users';
+
+    // Execute the query        
+    //$results = mysqli_query($conn, $sql_query);
+
+
+    if (isset($_POST['Submission'])) {
+        // retrieve the value 
+        session_start();
+        $email = trim($_POST['email']);
+        $sql_query = 'SELECT username FROM users WHERE email LIKE "%' . $email . '%"';
+        $results = mysqli_query($conn, $sql_query);
+        $user = mysqli_fetch_all($results, MYSQLI_ASSOC);
+
+        $_SESSION['userLogin'] = true;
+        if (isset($_SESSION['userLogin'])) {
+            var_dump($user);
+        } else {
+            echo 'Fucking Users';
+        }
+    }
+    //if (isset($_SESSION['page_view']))
+    //  $_SESSION['page_view'] += 1;  
+    // else
+    // $_SESSION['page_view'] =
     ?>
 
     <form action="" method="post">
-        <input type="text" name="email" placeholder="Email" value="<?php echo " $email" ?>"> <br><br>
-        <input type="text" name="password" placeholder="Password" value="<?php echo " $password" ?>"><br><br>
+        <input type="text" name="email" placeholder="Email" value=""> <br><br>
+        <input type=" text" name="password" placeholder="Password" value=""><br><br>
+
 
         <input type="submit" name="Submission">
     </form>
