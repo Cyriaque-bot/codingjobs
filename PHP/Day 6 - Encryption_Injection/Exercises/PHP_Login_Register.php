9831900df16ec453e8aef02331ab021b3f5e8835
InsteanAzeros <fctinstean@gmail.com>
2020-10-29 12:25:58 +0100

    encryption exercise

---
+++ PHP/Day 6 - Encryption_Injection/Exercises/PHP_Login_Register.php
@@ -0,0 +1,21 @@
+<?php
+	
+	/*
+		The purpose of this exercise is to create a register process for users.
+		
+		1. Create a 'register.php' page.
+		This page should display a form asking informations about the user (username, mail, password).
+
+		Once the submit button is clicked, you have to save the user in the database.
+		But don't forget to check the inputs ! 
+		It is mandatory to have a mail, a password and a nickname.
+		You also need to check for the mail to be a valid one.
+
+		ONLY IF everything is ok, you save the user in the DB.
+		ELSE you display a warning message.
+
+		!! IMPORTANT !! hash the password before saving in your database. You have to use 'password_hash()'.
+
+	*/
+
+?>
