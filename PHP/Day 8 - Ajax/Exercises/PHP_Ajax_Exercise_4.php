f78394e33685de7501d15adec02128310f4b3d3b
InsteanAzeros <fctinstean@gmail.com>
2020-11-03 14:04:46 +0100

    Bonus exercise ajax

---
+++ PHP/Day 8 - Ajax/Exercises/PHP_Ajax_Exercise_4.php
@@ -0,0 +1,12 @@
+<?php 
+
+/*
+
+	-- Exercise : 
+
+	In this exercise, you will continue Exercise_1.
+
+	Now, you must use JSON to communicate between Jquery and PHP.
+	Meaning : every 'echo' inside the PHP should display a JSON format.
+		
+*/
